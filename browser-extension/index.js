21324974db2b3b8da580bbd7128eb049961e5941
Jose Naime <josep.naimeg@gmail.com>
2021-07-29 22:59:04 -0500

    feat(fetch): fetch random quote

--- browser-extension/index.js
+++ browser-extension/index.js
@@ -1,12 +1,24 @@
 // Should only be triggered on first page load
-console.log('ho');
 
-$(function() {
-    $('#search-form').on("submit", function(e) {
+// Get Quote
+fetch("http://localhost:3030/quotes/random",
+    {
+        method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => data[0])
+    .then(quoteData => {
+        $('#quote-text').text(quoteData.text);
+        $('#quote-author').text('- ' + quoteData.author);
+        $('#quote-likes').text(quoteData.likes);
+    })
+
+$(function () {
+    $('#search-form').on("submit", function (e) {
         e.preventDefault(); // cancel the actual submit
         const searchInput = $('#search-input').val()
-        if (searchInput){
-            window.open('https://google.com/search?q='+searchInput)
+        if (searchInput) {
+            window.open('https://google.com/search?q=' + searchInput)
         }
     });
 });
